5004f520d81578c13235a17e5b21bb745cd5e34c
Marcelo Alejandro Boujón <77926233+boujonmarcelo@users.noreply.github.com>
2022-11-08 22:08:45 -0300

    Update AsistenciaOctubre.py
    Asistencia mes de Octubre de Marcelo Boujón

--- AsistenciaOctubre.py
+++ AsistenciaOctubre.py
@@ -39,3 +39,41 @@ operacion = Operacion(num1, num2)
 print(f'La suma de los dos numeros es: {operacion.suma()} \nLa multiplicacion de los dos numeros es: {operacion.multiplica()}')
 
 #------------------------------------------------------
+
+# ASISTENCIA MES DE OCTUBRE
+# THE JAVADABADEV'S
+
+# Alumno: Marcelo Alejandro Boujón
+
+class Aritmetica:
+    
+    #El nombre de este tipo de comentario es: DocString
+    #esto es documentacion de la clase en python
+    #Vamos a hacer en esta clase algunas operaciones de: suma, resta, multiplicacion y mas
+    
+    def __init__(self, operandoA, operandoB):
+        self.operandoA= operandoA
+        self.operandoB= operandoB
+
+       	def sumar(self):
+       	return  self.operandoA + self.operandoB
+
+        def resta(self):
+        return self.operandoA - self.operandoB
+
+        def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+        def dividir(self):
+        return self.operandoA / self.operandoB
+
+
+aritmetica1= Aritmetica(7, 9)    #pasamos los argumentos para los operandos
+
+print(f'La suma de los números es: {aritmetica1.sumar()}')
+print(f'La resta de los números es: {aritmetica1.resta()}')
+print(f'La multiplicación de los números es: {aritmetica1.multiplicar()}')
+print(f'La división de los números es: {aritmetica1.dividir():.2f}')
+
+-----------------------------------------------------------------------------------------
+
